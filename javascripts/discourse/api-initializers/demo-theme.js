46d170cd0aa16ba1bae590dae0aafba357f5b832
orhun-kolgeli <okolgel1@swarthmore.edu>
2021-07-02 16:04:29 +0300

    pluginAPI demo

--- javascripts/discourse/api-initializers/demo-theme.js
+++ javascripts/discourse/api-initializers/demo-theme.js
@@ -2,4 +2,10 @@ import { apiInitializer } from "discourse/lib/api";
 
 export default apiInitializer("0.11.1", api => {
   console.log("hello world from api initializer!");
+
+  const currentUser = api.getCurrentUser();
+
+  alert("Welcome back, " + currentUser.name + "!")
 });
+
+
