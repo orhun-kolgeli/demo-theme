65793a6ebdb7ce8bc17c86087114f6fccb0d52ae
orhun-kolgeli <okolgel1@swarthmore.edu>
2021-07-02 16:42:27 +0300

    get user info

--- javascripts/discourse/api-initializers/demo-theme.js
+++ javascripts/discourse/api-initializers/demo-theme.js
@@ -5,6 +5,7 @@ export default apiInitializer("0.11.1", api => {
   $( document ).ready(function() {
     const user = api.getCurrentUser();
     $('h1.hello-world').html("Hello " + user.name + "!");
+    console.log(user)
   });
 
 });
